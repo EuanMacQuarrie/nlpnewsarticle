a88ae19d43d38dfd6643a31e3f0fbd9cac7cb081
Euan MacQuarrie <ejmacq@gmail.com>
2020-05-28 23:41:11 -0400

    further changes to html and js files

--- src/server/index.js
+++ src/server/index.js
@@ -1,20 +1,28 @@
 const dotenv = require('dotenv');
 dotenv.config();
 console.log(`Your API key is ${process.env.API_KEY}`);
-
 var path = require('path')
 const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
 const mockAPIResponse = require('./mockAPI.js')
-
 const app = express()
+var aylien = require("aylien_textapi");
+// set aylien API credentias
+var aylien = require('aylien_textapi');
+var textapi = new aylien({
+    application_id: process.env.APP_ID,
+    application_key: process.env.APP_KEY
+  });
 
-app.use(express.static('dist'))
+app.use(express.static('dist'));
+app.use(bodyParser).json;
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(cors());
 
 console.log(__dirname)
-
 app.get('/', function (req, res) {
     res.sendFile('dist/index.html')
-    // res.sendFile(path.resolve('src/client/views/index.html'))
 })
 
 // designates what port the app will listen to for incoming requests
@@ -26,11 +34,4 @@ app.get('/test', function (req, res) {
     res.send(mockAPIResponse)
 })
 
-var aylien = require("aylien_textapi");
-
-// set aylien API credentias
-var AYLIENTextAPI = require('aylien_textapi');
-var textapi = new AYLIENTextAPI({
-    application_id: "b17085e0e3dd4aa6f671ccf85263830e",
-    application_key: "af90b4ef"
-  });
+projectData={};
