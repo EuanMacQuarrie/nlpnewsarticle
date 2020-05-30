ec574e68b68154fab5c16c17361e642af5f6458c
Euan MacQuarrie <ejmacq@gmail.com>
2020-05-30 15:30:44 -0400

    further changes to js

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,16 +1,25 @@
 async function handleSubmit(event) {
     event.preventDefault()
-
     // check what text was put into the form field
-    let formText = document.getElementById('name').value
-    Client.checkForName(formText)
-
+    let formInput = document.getElementById('url_main').value;
+    const valid = Client.checkForURL(formInput)
     console.log("::: Form Submitted :::")
-    fetch('http://localhost:8080/test')
-    .then(res => res.json())
-    .then(function(res) {
-        document.getElementById('results').innerHTML = res.message
-    })
+    if(!valid){
+        alert("Invalid URl");
+        return
+    }
+    console.log("The link is valid");
+    await fetch
+
+
+
+
+
+    // fetch('http://localhost:8080/test')
+    // .then(res => res.json())
+    // .then(function(res) {
+    //     document.getElementById('results').innerHTML = res.message
+    // })
 }
 
 export { handleSubmit }
