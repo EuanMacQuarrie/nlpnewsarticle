539e96ce19c35b7521ae7feb8ad02f1e63cdb546
Euan MacQuarrie <ejmacq@gmail.com>
2020-06-08 22:17:53 -0400

    more changes to javascript

--- src/client/js/urlChecker.test.js
+++ src/client/js/urlChecker.test.js
@@ -1,3 +1,3 @@
 import{ValidURL} from './urlChecker'; 
 
-describe('Testing to see if function "CheckForURL()" should be fucntioning')
+describe('Testing to see if function "CheckForURL()" should be functioning')
