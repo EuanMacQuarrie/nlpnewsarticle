e5696e6fa82b8346dc982edabc9e0d8d8e9ff319
Euan MacQuarrie <ejmacq@gmail.com>
2020-06-13 18:17:25 -0400

    docs ready for first submission

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,7 +1,7 @@
 async function handleSubmit(event) {
     event.preventDefault()
     // check what text was put into the form field
-    let formInput = document.getElementById('url_main').value;
+    let formInput = document.getElementById('submit').value;
     const valid = Client.ValidURL(formInput)
     console.log("::: Form Submitted :::")
     if(!valid){
