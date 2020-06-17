307923f383fdfee9b3e597598b8e9f14f2580f3c
Euan MacQuarrie <ejmacq@gmail.com>
2020-06-16 22:00:53 -0400

    prod files changed and chagnes to formhandler

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,7 +1,7 @@
 async function handleSubmit(event) {
     event.preventDefault()
     // check what text was put into the form field
-    let formInput = document.getElementById('submit').value;
+    let formInput = document.getElementById('url_main').value;
     const valid = Client.ValidURL(formInput)
     console.log("::: Form Submitted :::")
     if(!valid){
--- webpack.prod.js
+++ webpack.prod.js
@@ -5,7 +5,7 @@ const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const WorkboxPlugin = require('workbox-webpack-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
-// const TerserPlugin = require('terser-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
     entry: './src/client/index.js',
